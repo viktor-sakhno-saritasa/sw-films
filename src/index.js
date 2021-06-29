89afa6e2765c4be4c09824dc41a011e5b8f61495
Viktor Sakhno <viktor.sakhno@saritasa.com>
2021-06-29 18:21:51 +0700

    del config firebase

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyAdR7-yH8fqF99cdKPRMVGJlT_xsdRM9xc',
-  authDomain: 'sw-app-b0cf1.firebaseapp.com',
-  projectId: 'sw-app-b0cf1',
-  storageBucket: 'sw-app-b0cf1.appspot.com',
-  messagingSenderId: '1045485475856',
-  appId: '1:1045485475856:web:315f18bfca09d688ad1295',
-};
+// Config here
 
-firebase.initializeApp(firebaseConfig);
-
-const database = firebase.firestore();
-
-console.log(firebase);
-console.log(database.collection('films').get().then(data => console.log(data.data())));
+// firebase.initializeApp(firebaseConfig);
+// const database = firebase.firestore();

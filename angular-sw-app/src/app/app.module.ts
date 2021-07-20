421cf2c1b5b997410b6bb6ce57222d0e735dd1f5
Viktor Sakhno <viktor.sakhno@saritasa.com>
2021-07-20 10:00:13 +0700

    Init modules

--- angular-sw-app/src/app/app.module.ts
+++ angular-sw-app/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    LoginModule,
   ],
   providers: [],
   bootstrap: [AppComponent],

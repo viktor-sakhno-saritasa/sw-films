9c87622af06d75e2983444197437289c3d162d65
Viktor Sakhno <viktor.sakhno@saritasa.com>
2021-08-02 13:39:12 +0700

    Setting up deploy

--- angular-sw-app/src/environments/environment.prod.ts
+++ angular-sw-app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAdR7-yH8fqF99cdKPRMVGJlT_xsdRM9xc',
+    authDomain: 'sw-app-b0cf1.firebaseapp.com',
+    projectId: 'sw-app-b0cf1',
+    storageBucket: 'sw-app-b0cf1.appspot.com',
+    messagingSenderId: '1045485475856',
+    appId: '1:1045485475856:web:315f18bfca09d688ad1295',
+  },
 };
